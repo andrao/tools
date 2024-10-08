b74590d37a59e5c448af26407e5f251ffd9e2ff3
andrao <awmlake@gmail.com>
2024-10-08 09:33:23 +0300

    style(vitest): clean up config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,23 +1,19 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'vitest/config';
 
 /**
- * @const IS_INTEGRATION
  * @description Whether to run integration tests
  */
-const IS_INTEGRATION = Boolean(process.env.RUN_JEST_INTEGRATION_TESTS);
+const RUN_INTEGRATION_TESTS = Boolean(process.env.RUN_INTEGRATION_TESTS);
 
 /**
- * @const IGNORE_PATTERNS
  * @description Patterns to ignore in tests
  * - If _not_ integration, ignore integration test folder
  */
-const IGNORE_PATTERNS = ['node_modules', ...(IS_INTEGRATION ? [] : ['__integration_tests__'])];
+const IGNORE_PATTERNS = [
+    'node_modules',
+    ...(RUN_INTEGRATION_TESTS ? [] : ['__integration_tests__']),
+];
 
-/**
- * Jest config
- * @type {import("jest").Config}
- */
 export default defineConfig({
     test: {
         clearMocks: true,
